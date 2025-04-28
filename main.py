89b06e852fed7eafedd237256a7cc5a540412404
Joaoapbrito1 <joao.brito@zup.com.br>
2025-04-28 10:54:12 -0300

    feat: implements extra challenge, internal list in the SistemaRH class to store all registered employees, creates method called listar_bonuses, which displays the bonus of all employees.

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ def main():
     emp1 = RegularEmployee("João", 3000)
     emp2 = Manager("Maria", 5000, 1200)
 
+    hr.register_employee(emp1)
+    hr.register_employee(emp2)
+
     print("=== Bônus individuais ===")
     hr.show_bonus(emp1)
     hr.show_bonus(emp2)
+
+    print("\n=== Listando todos os bônus ===")
+    hr.list_bonuses()
 
 if __name__ == "__main__":
     main()
--- models/hr_system.py
+++ models/hr_system.py
@@ -7,6 +7,16 @@ def log_action(func):
     return wrapper
 
 class HRSystem:
+    def __init__(self):
+        self.employees = []
+
     @log_action
     def show_bonus(self, employee: Employee):
         print(f"{employee.get_name()} - Bonus: {employee.calculate_bonus()}")
+
+    def register_employee(self, employee: Employee):
+        self.employees.append(employee)
+
+    def list_bonuses(self):
+        for emp in self.employees:
+            self.show_bonus(emp)
